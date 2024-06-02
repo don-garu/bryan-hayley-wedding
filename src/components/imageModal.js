5f0f376fad041db9339c894504cbab8ce88beb02
bryan.ik <bryan.ik@kakaocorp.com>
2024-06-02 17:55:42 +0900

    이미지 수정

--- src/components/imageModal.js
+++ src/components/imageModal.js
@@ -1,7 +1,8 @@
 import { ReactComponent as LeftArrow } from '../assets/arrow-left.svg';
 import { ReactComponent as RightArrow } from '../assets/arrow-right.svg';
+import { useState } from "react";
 
-const imageModal = ({clickedImg, handleRotationRight, handleRotationLeft, setClickedImg}) => {
+const ImageModal = ({clickedImg, handleRotationRight, handleRotationLeft, setClickedImg}) => {
 
     const handleClick = (e) => {
         if (e.target.classList.contains("dismiss")) {
@@ -9,24 +10,48 @@ const imageModal = ({clickedImg, handleRotationRight, handleRotationLeft, setCli
         }
     }
 
-    return <>
-        <div className="overlay dismiss" onClick={handleClick}>
-            <img className="" src={clickedImg} alt="original size"/>
-            <span className="dismiss" onClick={handleClick}>X</span>
-            <div onClick={handleRotationRight}>
-                <div>
-                    <RightArrow className="overlay-arrows_right"
-                                width="50" height="50"/>
-                </div>
-            </div>
-            <div onClick={handleRotationLeft}>
-                <div>
-                    <LeftArrow className="overlay-arrows_left"
-                               width="50" height="50"/>
-                </div>
+    const [startX, setStartX] = useState(null);
+
+    const handleTouchStart = (e) => {
+        setStartX(e.touches[0].clientX);
+    };
+
+    const handleTouchMove = (e) => {
+        if (!startX) return;
+
+        const currentX = e.touches[0].clientX;
+        const diff = startX - currentX;
+
+        if (diff > 50) {
+            handleRotationRight();
+        } else if (diff < -50) {
+            handleRotationLeft();
+        }
+    };
+
+    const handleTouchEnd = () => {
+        setStartX(null);
+    };
+
+    return <div className='overlay'
+                onTouchStart={handleTouchStart}
+                onTouchMove={handleTouchMove}
+                onTouchEnd={handleTouchEnd}>
+        <img className="" src={clickedImg} alt="original size"/>
+        <span className="dismiss" onClick={handleClick}>X</span>
+        <div onClick={handleRotationRight}>
+            <div>
+                <RightArrow className="overlay-arrows_right"
+                            width="50" height="50"/>
             </div>
         </div>
-    </>;
+        <div onClick={handleRotationLeft}>
+            <div>
+                <LeftArrow className="overlay-arrows_left"
+                           width="50" height="50"/>
+            </div>
+        </div>
+    </div>
 };
 
-export default imageModal;
+export default ImageModal;
