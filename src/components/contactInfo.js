ce3bf726fd27a4a5812bbc3fae014d7b14e669be
bryan.ik <bryan.ik@kakaocorp.com>
2024-06-07 19:08:45 +0900

    전체 톤 변경

--- src/components/contactInfo.js
+++ src/components/contactInfo.js
@@ -5,37 +5,37 @@ import { ReactComponent as Message } from '../assets/mail-line.svg'
 const ContactInfo = () => {
     return <div className="contact-info">
         <div className='contact-info-block'>
-            <div className='contact-info-header'>신랑측</div>
-            <div className='contact-info-body'>
-                <div className='contact-info-name'>신랑 {contactData.groom.name}</div>
+            <div className='contact-info-header groom'>신랑측</div>
+            <div className='contact-info-body groom'>
+                <div className='contact-info-name no-deco'>신랑 {contactData.groom.name}</div>
                 <a href={`tel:${contactData.groom.number}`}><PhoneCall width='15' height='15'/></a>
                 <a href={`sms:${contactData.groom.number}`}><Message width='15' height='15'/></a>
             </div>
-            <div className='contact-info-body'>
-                <div className='contact-info-name'>아버지 {contactData.groom.father.name}</div>
+            <div className='contact-info-body groom'>
+                <div className='contact-info-name no-deco'>아버지 {contactData.groom.father.name}</div>
                 <a href={`tel:${contactData.groom.father.number}`}><PhoneCall width='15' height='15'/></a>
                 <a href={`sms:${contactData.groom.father.number}`}><Message width='15' height='15'/></a>
             </div>
-            <div className='contact-info-body'>
-                <div className='contact-info-name'>어머니 {contactData.groom.mother.name}</div>
+            <div className='contact-info-body groom'>
+                <div className='contact-info-name no-deco'>어머니 {contactData.groom.mother.name}</div>
                 <a href={`tel:${contactData.groom.mother.number}`}><PhoneCall width='15' height='15'/></a>
                 <a href={`sms:${contactData.groom.mother.number}`}><Message width='15' height='15'/></a>
             </div>
         </div>
         <div className='contact-info-block'>
-            <div className='contact-info-header'>신부측</div>
-            <div className='contact-info-body'>
-                <div className='contact-info-name'>신부 {contactData.bride.name}</div>
+            <div className='contact-info-header bride'>신부측</div>
+            <div className='contact-info-body bride'>
+                <div className='contact-info-name no-deco'>신부 {contactData.bride.name}</div>
                 <a href={`tel:${contactData.bride.number}`}><PhoneCall width='15' height='15'/></a>
                 <a href={`sms:${contactData.bride.number}`}><Message width='15' height='15'/></a>
             </div>
-            <div className='contact-info-body'>
-                <div className='contact-info-name'>아버지 {contactData.bride.father.name}</div>
+            <div className='contact-info-body bride'>
+                <div className='contact-info-name no-deco'>아버지 {contactData.bride.father.name}</div>
                 <a href={`tel:${contactData.bride.father.number}`}><PhoneCall width='15' height='15'/></a>
                 <a href={`sms:${contactData.bride.father.number}`}><Message width='15' height='15'/></a>
             </div>
-            <div className='contact-info-body'>
-                <div className='contact-info-name'>어머니 {contactData.bride.mother.name}</div>
+            <div className='contact-info-body bride'>
+                <div className='contact-info-name no-deco'>어머니 {contactData.bride.mother.name}</div>
                 <a href={`tel:${contactData.bride.mother.number}`}><PhoneCall width='15' height='15'/></a>
                 <a href={`sms:${contactData.bride.mother.number}`}><Message width='15' height='15'/></a>
             </div>
