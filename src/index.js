bcd880c80173bec3ea7f866d472b8767d894031c
bryan.ik <bryan.ik@kakaocorp.com>
2024-06-16 21:56:00 +0900

    Revert "캐시 적용?"
    This reverts commit 74ec62dcb232f5de57130bf055269500bb30779c.

--- src/index.js
+++ src/index.js
@@ -6,18 +6,15 @@ import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
 import { createRoot } from 'react-dom/client';
 
-import * as serviceWorker from './serviceWorker';
-
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
-    </React.StrictMode>
+    // <React.StrictMode>
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
+    // </React.StrictMode>
 );
-serviceWorker.register();
 
 reportWebVitals(sendToVercelAnalytics);
