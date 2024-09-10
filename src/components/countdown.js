d338e8e5492ac4b564720c26a883c6dca6693afe
bryan.ik <bryan.ik@kakaocorp.com>
2024-09-11 06:51:17 +0900

    d-day 설정 추가

--- src/components/countdown.js
+++ src/components/countdown.js
@@ -1,10 +1,27 @@
 import { useEffect, useState } from "react";
 
+const thisIsNotTheDay = (timeLeft) => {
+    return <div className='calendar-countdown-text'>
+        준익 <span className='calendar-countdown-text-inner'>♥</span> 현진의 결혼까지<br/>
+        {timeLeft.days}일 {timeLeft.hours}시간 {timeLeft.minutes}분 {timeLeft.seconds}초 남았어요
+    </div>
+}
+
+const thisIsTheDay = () => {
+    return <div className='calendar-countdown-text'>
+        준익 <span className='calendar-countdown-text-inner'>♥</span> 현진의 결혼이 <b>바로 오늘</b>입니다!
+    </div>
+}
+
 const Countdown = () => {
     const targetDate = new Date("2024-09-29 11:00")
+    let isTheDay = false;
 
     const calculateTimeLeft = () => {
         const now = new Date()
+        if (now.getMonth() === targetDate.getMonth() && now.getDate() === targetDate.getDate()) {
+            isTheDay = true
+        }
 
         const difference = targetDate - now
 
@@ -37,10 +54,7 @@ const Countdown = () => {
 
     return (
         <div className='calendar-countdown-container'>
-            <div className='calendar-countdown-text'>
-                준익 <span className='calendar-countdown-text-inner'>♥</span> 현진의 결혼까지<br/>
-                {timeLeft.days}일 {timeLeft.hours}시간 {timeLeft.minutes}분 {timeLeft.seconds}초 남았어요
-            </div>
+            {isTheDay ? thisIsTheDay() : thisIsNotTheDay(timeLeft)}
         </div>
     );
 };
